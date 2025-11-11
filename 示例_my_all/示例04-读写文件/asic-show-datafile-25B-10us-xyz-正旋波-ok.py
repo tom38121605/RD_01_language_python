5dd92115b8b23e19c8e40fd1f79d2bb9a0cea67d
tom38121605 <13538121605@139.com>
2025-11-11 18:05:40 +0800

    111

---
+++ 示例_my_all/示例04-读写文件/asic-show-datafile-25B-10us-xyz-正旋波-ok.py
@@ -0,0 +1,81 @@
+
+import time
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal
+
+x1points = []
+y1points = []
+z1points = []
+t1points = []
+
+DATAFILE1=".\\data01.dat"
+DATAFILE2=".\\data02.txt"
+
+f1=open(DATAFILE1,"rb")
+f2=open(DATAFILE2,"w")
+
+idatanum = 0
+idatanum2 = 0
+
+try:
+    while True:
+        if idatanum <= 10000:
+            idatanum += 1
+            received_data = f1.read(13)
+            if not received_data:
+                break
+            print(received_data)
+            # print(idatanum)
+
+            # if received_data[:1] == b'\xaa':
+            if received_data[0] == 0xaa:
+               print("x1 ", int.from_bytes(received_data[1:4], "big", signed="True"), file=f2)
+
+               x1points.append(int.from_bytes(received_data[1:4], byteorder='big', signed="True"))
+               y1points.append(int.from_bytes(received_data[4:7], byteorder='big', signed="True"))
+               z1points.append(int.from_bytes(received_data[7:10], byteorder='big', signed="True"))
+               t1points.append(int.from_bytes(received_data[10:13], byteorder='big', signed="True"))
+
+               idatanum2 +=1
+               # print(idatanum2)
+
+        else:
+            break
+
+    f1.close()
+    f2.close()
+
+    plt.plot(x1points, color='red')
+    plt.plot(y1points, color='green')
+    plt.plot(z1points, color='blue')
+
+    plt.title('x -- red,  y -- green,  z -- blue ')
+    plt.axis([0,100,-350000, 80000])
+    plt.show()
+    
+    # apply Walch power function to raw ADC data set  
+    fft_len = 1024 * 8
+    xF, spectrum = scipy.signal.welch(x1points, fs=500, nperseg=fft_len, noverlap=0)       
+    freq_resp = 20*(np.log10(np.sqrt(spectrum)))
+
+
+    ax = plt.subplot()
+    plt.title('I3C Captured Data Spectral Plot')
+    ax.plot(xF,freq_resp, color='black', linewidth=1) # plot raw spectral data
+    #ax.plot(limited_freqs, n_floor, color='red', linewidth=1) # plot raw spectral data
+    ax.set_xscale('log')
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('X-Channel Output Data [dB]')
+    plt.grid(visible=True, which='both', axis='both')
+    plt.axis([1, 250, 0,120])
+    plt.grid(which='major', color='black', linestyle='-', linewidth = 0.75)
+    plt.grid(which='minor', color='black', linestyle='-', linewidth = 0.25)
+
+    plt.show()   
+
+except FileNotFoundError:
+    print("Error: data01.dat file not found。")
+except Exception as e:
+    print(f"An unknown error occurred：{e}")
