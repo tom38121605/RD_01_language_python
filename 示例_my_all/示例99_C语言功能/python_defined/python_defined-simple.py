b1246708d0206a0b51fdce1be9c9d15cc8bfd5d1
tom38121605 <13538121605@139.com>
2026-04-30 18:38:38 +0800

    111

---
+++ 示例_my_all/示例99_C语言功能/python_defined/python_defined-simple.py
@@ -0,0 +1,18 @@
+﻿
+
+DEFINES = \
+{
+    # "MODE_X": True,
+    "MODE_XYZ": True,
+}
+
+def defined(macro_name):
+    is_defined = macro_name in DEFINES
+    return is_defined
+
+if defined("MODE_X"):
+    print("use x")
+
+elif defined("MODE_XYZ"):
+    print("use xyz")
+
